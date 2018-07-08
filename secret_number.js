87fd478d35ea1ad69680d2d6fe321a381e5202f9
Harsh Kotak <kotak_harsh@hotmail.com>
2018-07-07 15:33:07 -1000

    completed exercise

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
-};
+    var num = Math.floor(Math.random() * Math.floor(1000001));
+    function secretNumber() {
+        // console.log(num);
+        return num;
+    }
+    return secretNumber;
+    };
+
+        
+    
+
